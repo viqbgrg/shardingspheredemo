4107285759636543c2c2914dbb1ff5a4793a9cb1
viqbgrg <viqbgrg@gmail.com>
2021-06-22 11:15:43 +0800

    first commit

---
+++ src/main/java/com/example/shardingspheredemo/ShardingspheredemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.shardingspheredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingspheredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingspheredemoApplication.class, args);
+	}
+
+}
